cd5c26bd8ded20a6a5b7984b5bccaf4fd28ca535
Noctusy <noctusy@gmail.com>
2016-05-25 09:30:12 +0200

    Ajout de la CoToDB avec l'IHM

---
+++ ProjectCODEX/src/main/java/dbconnec/CoToDB.java
@@ -0,0 +1,58 @@
+package dbconnec;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+/**
+ *
+ * @author norxciel
+ */
+public class CoToDB {
+
+    // création d'une instance typée CoToDB pour le singleton.
+    private static volatile CoToDB instance = null;
+    private static Connection DBConnect = null;
+
+    // Constructeur pour établir la connection à la base de donnée.
+    private CoToDB() throws SQLException {
+        String pathDB = "";
+
+        JFileChooser fileChooser = new JFileChooser();
+        
+        // Entrée des filtres, puis application du filtre.
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("SQLite", "sqlite");
+        fileChooser.setFileFilter(filter);
+
+        int returnVal = fileChooser.showOpenDialog(null);
+
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile(); // Stockage du fichier sélectionné par l'utilisateur.
+            pathDB = file.getAbsolutePath();           // Stockage du path dans la variable.
+            CoToDB.DBConnect = DriverManager.getConnection(pathDB);
+        }
+    }
+
+    // Methode d'obtention d'instance de la connection à la base de donnée.
+    private static CoToDB getInstance() {
+        if (instance == null) {
+            try {
+                instance = new CoToDB();
+            } catch (SQLException ex) {
+                Logger.getLogger(CoToDB.class.getName()).log(Level.SEVERE, null, ex);
+                System.out.println("Erreur de connection à la BDD");
+            }
+        }
+        
+        return instance;
+    }
+    
+    private static Connection getConnexion(){
+        return DBConnect;
+    }
+}
